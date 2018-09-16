0fc58e0e2111cd257256c611373a90cb559fb5c1
guocai0921 <923055433@qq.com>
2018-09-17 07:53:33 +0800

    v1.1.3

--- it-manager/it-manager-pojo/src/main/java/com/guocai/pojo/TbContentCategoryExample.java
+++ it-manager/it-manager-pojo/src/main/java/com/guocai/pojo/TbContentCategoryExample.java
@@ -1,11 +1,15 @@
 package com.guocai.pojo;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class TbContentCategoryExample {
-    protected String orderByClause;
+public class TbContentCategoryExample implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	protected String orderByClause;
 
     protected boolean distinct;
 
