c1cb16879480135cf4b61e8c2342e09def5d8418
guocai0921 <923055433@qq.com>
2018-09-22 13:11:11 +0800

    v1.2.8

---
+++ it-manager/it-manager-service/src/main/java/com/guocai/service/TbItemParamItemService.java
@@ -0,0 +1,5 @@
+package com.guocai.service;
+
+public interface TbItemParamItemService {
+	public String getItemParamByItemId(long itemId);
+}
---
+++ it-manager/it-manager-service/src/main/java/com/guocai/service/impl/TbItemParamItemServiceImpl.java
@@ -0,0 +1,16 @@
+package com.guocai.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.guocai.service.TbItemParamItemService;
+
+@Service
+public class TbItemParamItemServiceImpl implements TbItemParamItemService {
+
+	@Override
+	public String getItemParamByItemId(long itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
